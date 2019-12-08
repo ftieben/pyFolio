9423c5a3f53b38415a096d245f664d5d3da21412
FTieben <ftiebe@gmail.com>
2019-12-08 14:44:13 +0100

    its threaded

--- pyFolio/main.py
+++ pyFolio/main.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import yfinance as yf
 import pf_influx as influx
+import concurrent.futures
 
 
 def readPortfolio():
@@ -11,19 +12,21 @@ def readPortfolio():
     return df
 
 
-def getData(df):
-    for index, row in df.iterrows():
-        extract = yf.Ticker(row["name"])
-        row["price"] = extract.info["regularMarketOpen"]
-        row["sum"] = row["price"] * row["amount"]
-        df.loc[index, "price"] = row["price"]
-        df.loc[index, "sum"] = row["sum"]
-        influx.write_row_to_influx(row)
-    return df
+def getData(row):
+    extract = yf.Ticker(row["name"])
+    row["price"] = extract.info["regularMarketOpen"]
+    row["sum"] = row["price"] * row["amount"]
+    influx.write_row_to_influx(row)
+    return True
 
 
 def main():
-   getData(readPortfolio())
+    df = readPortfolio()
+    threads = len(df.index)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        for index, row in df.iterrows():
+            executor.submit(getData, row)
+    executor.shutdown(wait=True)
 
 
 if __name__ == '__main__':
